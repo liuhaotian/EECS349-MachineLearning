12ba0d4b87a3b7f529abf7693484feadfb8c8523
Haotian Liu <liuhaotian.air@gmail.com>
2012-10-06 18:47:04 -0500

    Disp inputs

--- HW1/decisiontree.m
+++ HW1/decisiontree.m
@@ -1,5 +1,8 @@
 function decisiontree (inputFileName, trainingSetSize, numberOfTrials, verbose)
-	disp(sprintf('Input:   %s\n', inputFileName));
+	disp(sprintf('Input File Name : %s\n', inputFileName));
+	disp(sprintf('Traning Set Size: %d\n', trainingSetSize));
+	disp(sprintf('Number Of Trials: %s\n', numberOfTrials));
+	disp(sprintf('Verbose Mode    : %s\n', verbose));
 
 
 
