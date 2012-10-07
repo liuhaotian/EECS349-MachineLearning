9418143a767ec9a774f6f88b6ada35db18bc5d71
Haotian Liu <liuhaotian.air@gmail.com>
2012-10-07 01:50:15 -0500

    Calc entropy

--- HW1/entropy.m
+++ HW1/entropy.m
@@ -1,1 +1,7 @@
 function ret = entropy (examples, attribute)
+	examplestrue = examples(find(examples(:,attribute)>0),:);
+	examplesfalse = examples(find(examples(:,attribute)<1),:);
+	pture = length(examplestrue) / length(examples);
+	pfalse = length(examplesfalse) / length(examples);
+	ret = - pture * log2(pture) - pfalse * log2(pfalse);
+end
