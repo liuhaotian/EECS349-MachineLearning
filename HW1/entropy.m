895111c060711eba45bb79aec950b4a8856ab4f5
Haotian Liu <liuhaotian.air@gmail.com>
2012-10-07 02:18:21 -0500

    Fix entropy

--- HW1/entropy.m
+++ HW1/entropy.m
@@ -1,6 +1,6 @@
-function ret = entropy (examples, attribute)
-	examplestrue = examples(find(examples(:,attribute)>0),:);
-	examplesfalse = examples(find(examples(:,attribute)<1),:);
+function ret = entropy (examples, target_attr)
+	examplestrue = examples(find(examples(:,target_attr)>0),:);
+	examplesfalse = examples(find(examples(:,target_attr)<1),:);
 	pture = length(examplestrue) / length(examples);
 	pfalse = length(examplesfalse) / length(examples);
 	ret = - pture * log2(pture) - pfalse * log2(pfalse);
