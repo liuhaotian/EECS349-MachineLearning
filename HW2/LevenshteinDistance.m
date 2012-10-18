c13ce4c2a7c033cb1cd87c5e6e8d2b184e0327ae
Haotian Liu <liuhaotian.air@gmail.com>
2012-10-17 22:02:56 -0500

    Build the table

--- HW2/LevenshteinDistance.m
+++ HW2/LevenshteinDistance.m
@@ -1,7 +1,17 @@
-function dist = LevenshteinDistance( s,t,deletionCost,insertionCost,substitutionCost )
-    dist = 0;
-    
+function dist = LevenshteinDistance(s, t, deletionCost, insertionCost, substitutionCost)
+	
+	%	init the return value
+	dist = 0;
+
+	%	get the s,t info
+	s_size = length(s);
+	t_size = length(t);
+
+	%	build the dist matrix
+	d = zeros(s_size + 1, t_size + 1);
+	d(1:s_size + 1, 1) = [0:s_size] * deletionCost;
+	d(1, 1:t_size + 1) = [0:t_size] * insertionCost;
+ 
 
 
 end
-
