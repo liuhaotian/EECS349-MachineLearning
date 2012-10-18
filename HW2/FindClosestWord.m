22536924bc960513776e70bdd636986a3c8f6db0
Haotian Liu <liuhaotian.air@gmail.com>
2012-10-17 23:19:18 -0500

    Find closest word

--- HW2/FindClosestWord.m
+++ HW2/FindClosestWord.m
@@ -1,6 +1,24 @@
-function word_out = LevenshteinDistance(word_in, dict_array)
+function word_out = FindClosestWord(word_in, dict_array)
 	%	init the word_out
-	word_out = word_in;
+	word_out = dict_array{1};
 
-	
+	%	get the length of dict, word
+	dict_size = length(dict_array);
+	word_size = length(word_in);
+
+	%	set the cost
+	deletionCost = 2;
+	insertionCost = 2;
+	substitutionCost = 2;
+
+
+	mini = word_size * deletionCost;
+	for i = 1:dict_size
+		if length(dict_array{i}) < mini / insertionCost + word_size || length(dict_array{i}) > word_size - mini / deletionCost
+			mini = min(LevenshteinDistance(word_in, dict_array{i}, deletionCost, insertionCost, substitutionCost),mini);
+			word_out = dict_array{i};
+		end
+	end
+
+
 end
