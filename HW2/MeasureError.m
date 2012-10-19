831ae0c539054bd319060e2aca81017018d50a52
Haotian Liu <liuhaotian.air@gmail.com>
2012-10-18 22:16:51 -0500

    Measure Error

--- HW2/MeasureError.m
+++ HW2/MeasureError.m
@@ -1,6 +1,21 @@
 function errorRate = MeasureError(typo, trueword, dictionary)
 	%	init the return value
 	errorRate = 0;
+	errorCount = 0;
 
+	%	check input
+	typo_size = length(typo);
+	true_size = length(trueword);
+	if typo_size ~= true_size
+		disp('MeasureError.m: input size not match!');
+		return
+	end
 
+	for i = 1:typo_size
+		if trueword(i) ~= FindClosestWord(typo(i), dictionary)
+			errorCount = errorCount + 1;
+		end
+	end
+
+	errorRate = errorCount / typo_size;
 end
