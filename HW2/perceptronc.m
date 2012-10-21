fae0a8da9775b76bd148525c239c899a1f9c683e
Haotian Liu <liuhaotian.air@gmail.com>
2012-10-21 17:05:10 -0500

    Got the plot

--- HW2/perceptronc.m
+++ HW2/perceptronc.m
@@ -8,10 +8,11 @@ function [w,k] = perceptronc(w_init,X,Y)
 	w = [w(:,1), w(:,1).^2, w(:,2)];
 
 	%	Sequential Perceptron Algo
-	while sum(sign(polyval(w, X(:,1))) ~= Y)
+	while sum(sign(w*X')' ~= Y)
 		k = mod(k, length(X)) + 1;
-		if sign(polyval(w, X(k,1))) ~= Y(k)
+		if sign(w*X(k,:)') ~= Y(k)
 			w = w + X(k,:) * Y(k);
+            plot(X(Y==1,1),X(Y==1,2),'o',X(Y==-1,1),X(Y==-1,2),'o',X(:,1),-w(3)/w(2)-w(1)/w(2)*X(:,1),'x');
 		end 
 	end
 
