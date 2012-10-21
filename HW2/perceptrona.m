e39d2dcf974650f8ade2c799419860af578387ed
Haotian Liu <liuhaotian.air@gmail.com>
2012-10-21 11:58:44 -0500

    Fix bug

--- HW2/perceptrona.m
+++ HW2/perceptrona.m
@@ -4,7 +4,7 @@ function [w,k] = perceptrona(w_init,X,Y)
 	k = 0;
 
 	%	Sequential Perceptron Algo
-	while sum(xor(((X > w) - (X < w) + 2) / 2, (Y + 2) / 2))
+	while sum(xor(((X > w) - (X < w) + 1) / 2, (Y + 1) / 2))
 		k = mod(k, length(X)) + 1;
 		if polyval(w, X(k)) ~= Y(k)
 			w = w + X(k) .* Y(k);
