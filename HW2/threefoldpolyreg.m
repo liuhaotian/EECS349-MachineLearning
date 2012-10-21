fce2e8a41bdfa2afda7655049def6603ae048b2d
Haotian Liu <liuhaotian.air@gmail.com>
2012-10-21 00:43:13 -0500

    Fix bug

--- HW2/threefoldpolyreg.m
+++ HW2/threefoldpolyreg.m
@@ -6,15 +6,15 @@ function threefoldpolyreg(k)
 
 	foldarray = zeros(length(X),1);
 	while sum(foldarray == 1) ~= length(X)/3
-		foldarray(random('unid',length(X)) = 1;
+		foldarray(random('unid',length(X))) = 1;
 	end
 	while sum(foldarray == 2) ~= sum(foldarray ~= 1)/2
-		therand = random('unid',length(X);
+		therand = random('unid',length(X));
 		foldarray(therand) = 2 - foldarray(therand);
 	end
 	for i = 1:length(X)
 		if ~ foldarray(i)
-			foldarray(i) = 3
+			foldarray(i) = 3;
 		end
 	end
 
