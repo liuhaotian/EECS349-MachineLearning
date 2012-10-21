7ebd3c84170085d5f3c8c8d6b8aa75454ae36232
Haotian Liu <liuhaotian.air@gmail.com>
2012-10-21 11:01:01 -0500

    Done the algo

--- HW2/perceptrona.m
+++ HW2/perceptrona.m
@@ -1,4 +1,15 @@
 function [w,k] = perceptrona(w_init,X,Y)
+	%	initialize w k
+	w = w_init;
+	k = 0;
 
+	%	Sequential Perceptron Algo
+	while polyval(w, X) ~= Y
+		k = mod(k, length(X)) + 1;
+		if polyval(w, X(k)) ~= Y(k)
+			w = w + [X(k), Y(k)] .* Y(k);
+			plot(X,Y,,'o',X,polyval(w,X),'x');
+		end 
+	end
 
 end
