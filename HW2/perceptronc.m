41177b57aaa996e42b2d56cdb3e478c984f9adcd
Haotian Liu <liuhaotian.air@gmail.com>
2012-10-21 15:52:17 -0500

    Init perceptronc

---
+++ HW2/perceptronc.m
@@ -0,0 +1,14 @@
+function [w,k] = perceptronc(w_init,X,Y)
+	%	initialize w k
+	w = w_init;
+	k = 0;
+
+	%	Sequential Perceptron Algo
+	while sum(sign(polyval(w, X(:,1))) ~= Y)
+		k = mod(k, length(X)) + 1;
+		if sign(polyval(w, X(k,1))) ~= Y(k)
+			w = w + X(k,:) * Y(k);
+		end 
+	end
+
+end
