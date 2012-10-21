9e0c4673f13f6e98d087d7636b6c64f34a086789
Haotian Liu <liuhaotian.air@gmail.com>
2012-10-21 17:08:19 -0500

    Get the interation

--- HW2/perceptronc.m
+++ HW2/perceptronc.m
@@ -3,15 +3,15 @@ function [w,k] = perceptronc(w_init,X,Y)
 	w = w_init;
 	k = 0;
 
-	%	transform
+	%	transform, (x,x^2) 
 	X = [X(:,1), X(:,1).^2, X(:,2)];
 	w = [w(:,1), w(:,1).^2, w(:,2)];
 
 	%	Sequential Perceptron Algo
 	while sum(sign(w*X')' ~= Y)
-		k = mod(k, length(X)) + 1;
-		if sign(w*X(k,:)') ~= Y(k)
-			w = w + X(k,:) * Y(k);
+		k = k + 1;
+		if sign(w*X(mod(k, length(X)) + 1,:)') ~= Y(mod(k, length(X)) + 1)
+			w = w + X(mod(k, length(X)) + 1,:) * Y(mod(k, length(X)) + 1);
             plot(X(Y==1,1),X(Y==1,2),'o',X(Y==-1,1),X(Y==-1,2),'o',X(:,1),-w(3)/w(2)-w(1)/w(2)*X(:,1),'x');
 		end 
 	end
