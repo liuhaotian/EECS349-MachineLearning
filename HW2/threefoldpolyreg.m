ce892c9cd0e457f0021df4ed47c6feaea2a3854b
Haotian Liu <liuhaotian.air@gmail.com>
2012-10-21 00:41:45 -0500

    Generate the 3-fold

--- HW2/threefoldpolyreg.m
+++ HW2/threefoldpolyreg.m
@@ -1,5 +1,21 @@
 function threefoldpolyreg(k)
 	%	load the input
-	load linearclass.mat
+	load linearreg.mat
+
+	%	build the fold
+
+	foldarray = zeros(length(X),1);
+	while sum(foldarray == 1) ~= length(X)/3
+		foldarray(random('unid',length(X)) = 1;
+	end
+	while sum(foldarray == 2) ~= sum(foldarray ~= 1)/2
+		therand = random('unid',length(X);
+		foldarray(therand) = 2 - foldarray(therand);
+	end
+	for i = 1:length(X)
+		if ~ foldarray(i)
+			foldarray(i) = 3
+		end
+	end
 
 end
