aab253ef7669d36423b90ee5d9742cfec7e8c798
Haotian Liu <liuhaotian.air@gmail.com>
2012-10-21 15:53:54 -0500

    The change

--- HW2/perceptronc.m
+++ HW2/perceptronc.m
@@ -3,6 +3,9 @@ function [w,k] = perceptronc(w_init,X,Y)
 	w = w_init;
 	k = 0;
 
+	%	transform
+	X = abs(X - 1.5);
+
 	%	Sequential Perceptron Algo
 	while sum(sign(polyval(w, X(:,1))) ~= Y)
 		k = mod(k, length(X)) + 1;
