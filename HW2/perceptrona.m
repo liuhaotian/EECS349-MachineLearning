1a981b52be201d2a174bf6b37c81718ea3679ea1
Haotian Liu <liuhaotian.air@gmail.com>
2012-10-21 11:15:10 -0500

    Fix bug

--- HW2/perceptrona.m
+++ HW2/perceptrona.m
@@ -7,7 +7,7 @@ function [w,k] = perceptrona(w_init,X,Y)
 	while sum(polyval(w, X) ~= Y)
 		k = mod(k, length(X)) + 1;
 		if polyval(w, X(k)) ~= Y(k)
-			w = w + [X(k), Y(k)] .* Y(k);
+			w = w + X(k) .* Y(k);
 			plot(X,Y,'o',X,polyval(w,X),'x');
 		end 
 	end
