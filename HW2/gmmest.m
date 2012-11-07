cb8a9f46360d91f60a3251a36fd1d95a5d05a326
Haotian Liu <liuhaotian.air@gmail.com>
2012-11-06 20:21:26 -0600

    Calc the gaussian dist

--- HW2/gmmest.m
+++ HW2/gmmest.m
@@ -1,3 +1,7 @@
 function [mu, sigmasq, wt, L] = gmmest(X, mu_init, sigmasq_init, wt_init, its)
-
+    pre_mu      = mu_init;
+    pre_sigmasq = sigmasq_init;
+    pre_wt      = wt_init;
+    %for i = 1:its
+        p = exp(-(ones(size(pre_mu))' * X .- pre_mu' * ones(size(X))).^2 ./ 2 ./ (pre_sigmasq * ones(size(X)))) ./ sqrt(2 .* pi .* (pre_sigmasq * ones(size(X))));
 end
